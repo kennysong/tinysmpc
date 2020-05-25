0d4367f74d8ba4e27ec6a6031ead6b30e197fcef
Kenny Song <kenny.ysong@gmail.com>
2020-05-25 16:25:05 +0900

    Add support for naive arithmetic directly on Shares (needed for SecureNN protocol). Refactored SharedScalar arithmetic and added more comments.

--- micro_smpc/micro_smpc.py
+++ micro_smpc/micro_smpc.py
@@ -1,8 +1,10 @@
 from random import choice, randrange
 
-Q = 18446744073709551557
+Q = 18446744073709551557  # Largest prime below 2^64
 
 class VirtualMachine():
+    '''A very simple class that represents a machine. 
+       It has a name and collects objects (PrivateScalars and Shares).'''
     def __init__(self, name):
         self.name = name
         self.objects = []
@@ -13,12 +15,14 @@ class VirtualMachine():
         return string
 
 class PrivateScalar():
+    '''A class that represents a (secret) number that belongs to a machine.'''
     def __init__(self, value, owner):
         self.value = value
         self.owner = owner
         owner.objects.append(self)
 
     def share(self, machines, Q=Q):
+        '''Generate additive secret shares of self.value, and distribute them to machines.'''
         # Make sure we're sharing across unique machines
         assert self.owner not in machines
         assert len(machines) == len(set(machines))
@@ -28,7 +32,7 @@ class PrivateScalar():
         values.append((self.value - sum(values)) % Q)
         
         # Give one share to each machine
-        shares = [Share(value, machine) for value, machine in
+        shares = [Share(value, machine, Q=Q) for value, machine in
                   zip(values, machines + [self.owner])]
         
         # Return a SharedScalar that tracks all of these shares
@@ -38,23 +42,72 @@ class PrivateScalar():
         return f'PrivateScalar({self.value}, \'{self.owner.name}\')'
             
 class Share():
-    def __init__(self, value, owner):
+    '''A class that represents a secret share that belongs to a machine.
+       It supports normal arithmetic with other Shares or integers (+, -, *).'''
+    def __init__(self, value, owner, Q=Q):
+        assert 0 <= value < Q
         self.value = value
         self.owner = owner
+        self.Q = Q
         owner.objects.append(self)
     
+    # Called by: self + other
+    def __add__(self, other):
+        # Addition of a Share and a public integer known to all machines
+        if isinstance(other, int):
+            sum_value = (self.value + other) % self.Q
+            
+        # Addition of two Shares
+        elif isinstance(other, Share):
+            assert self.owner == other.owner
+            sum_value = (self.value + other.value) % self.Q
+            
+        return Share(sum_value, self.owner, self.Q)
+    
+    # Called by: other + self (when other is not a Share)
+    def __radd__(self, other):
+        return self.__add__(other)
+    
+    # Called by: self - other
+    def __sub__(self, other):
+        return self.__add__(-1*other)
+    
+    # Called by: other - self (when other is not a Share)
+    def __rsub__(self, other):
+        return (-1*self).__add__(other)
+    
+    # Called by: self * other
+    def __mul__(self, other):
+        # Multiplication of a Share and a public integer known to all machines
+        if isinstance(other, int):
+            prod_value = (self.value * other) % self.Q
+            
+        # Multiplication of two Shares
+        elif isinstance(other, Share):
+            assert self.owner == other.owner
+            prod_value = (self.value * other.value) % self.Q
+    
+        return Share(prod_value, self.owner, self.Q)
+    
+    # Called by: other * self (when other is not a Share)  
+    def __rmul__(self, other):
+        return self.__mul__(other)
+    
     def __repr__(self):
-        return f'Share({self.value}, \'{self.owner.name}\')'
+        return f'Share({self.value}, \'{self.owner.name}\', {self.Q})'
     
 class SharedScalar():
+    '''A class that tracks all secret shares that corresponds to one PrivateScalar.
+       It supports *secure* arithmetic with other SharedScalars or integers (+, -, *).'''
     def __init__(self, shares, Q=Q):
+        assert all(share.Q == Q for share in shares)
         self.shares = shares
         self.owners = {share.owner for share in shares}
         self.share_of = {share.owner: share for share in shares}
         self.Q = Q
         
     def reconstruct(self, owner):
-        assert owner in self.owners        
+        '''Send all secret shares to owner (can be anyone), and reconstruct the PrivateScalar value on that machine.'''
         values = [share.value for share in self.shares]
         value = sum(values) % self.Q
         return PrivateScalar(value, owner)
@@ -63,17 +116,16 @@ class SharedScalar():
     def __add__(self, other):
         # Addition of a SharedScalar and a public integer known to all machines
         if isinstance(other, int):
-            new_share = Share(self.shares[0].value + other, self.shares[0].owner)
+            # To do the addition, we add the integer to one (random) share only
+            new_share = self.shares[0] + other
             return SharedScalar([new_share] + self.shares[1:], Q=self.Q)
         
         # Addition of two SharedScalars
         elif isinstance(other, SharedScalar):
+            # To do the addition, we add each machine's shares together
             assert self.owners == other.owners
-            sum_shares = []
-            for owner in self.owners:
-                self_share, other_share = self.share_of[owner], other.share_of[owner] 
-                sum_share = Share((self_share.value + other_share.value) % self.Q, owner)
-                sum_shares.append(sum_share)
+            sum_shares = [self.share_of[owner] + other.share_of[owner]
+                          for owner in self.owners]
             return SharedScalar(sum_shares, Q=self.Q)
         
     # Called by: other + self (when other is not a SharedScalar)
@@ -92,15 +144,17 @@ class SharedScalar():
     def __mul__(self, other):
         # Multiplication of a SharedScalar and a public integer known to all machines
         if isinstance(other, int):
-            prod_shares = [Share(share.value * other, share.owner) 
-                           for share in self.shares]
+            # To do the multiplication, we multiply the integer with all shares
+            prod_shares = [share * other for share in self.shares]
             return SharedScalar(prod_shares, Q=self.Q)
             
         # Multiplication of two SharedScalars
         elif isinstance(other, SharedScalar):
+            # To do the multiplication, we do the SPDZ protocol as described in:
+            # https://bristolcrypto.blogspot.com/2016/10/what-is-spdz-part-2-circuit-evaluation.html
             assert self.owners == other.owners
 
-            # Generate a random multiplication triple
+            # Generate a random multiplication triple (public)
             a, b = randrange(self.Q), randrange(self.Q)
             c = (a * b) % self.Q
 
@@ -111,11 +165,11 @@ class SharedScalar():
             shared_b = PrivateScalar(b, rand_owner).share(other_owners, Q=self.Q)
             shared_c = PrivateScalar(c, rand_owner).share(other_owners, Q=self.Q)
 
-            # Compute (shared) self - a, other - b
+            # Compute self - a, other - b (shared)
             shared_self_m_a = self - shared_a
             shared_other_m_b = other - shared_b
 
-            # Reconstruct (public) self - a, other - b
+            # Reconstruct self - a, other - b (public)
             self_m_a = shared_self_m_a.reconstruct(rand_owner).value
             other_m_b = shared_other_m_b.reconstruct(rand_owner).value
             
