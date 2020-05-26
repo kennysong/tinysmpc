3a4fdd6864134d22f6bfba0a2405b7016bf9ff0a
Kenny Song <kenny.ysong@gmail.com>
2020-05-26 21:52:11 +0900

    A halfway commit. Made secret sharing work over the int64s in addition to integers mod prime. Who knew int64 was a finite ring? Needs more refactoring.

--- tinysmpc/tinysmpc.py
+++ tinysmpc/tinysmpc.py
@@ -1,6 +1,41 @@
-from random import choice, randrange
+from random import choice, randint, randrange
 
-Q = 18446744073709551557  # Largest prime below 2^64
+# Mathematical note:
+#
+# For additive secret sharing to work, we need all of the numbers we're working with
+# to be in a finite abelian group under addition. [1] 
+# 
+# (Technically, for SMPC with additive secret sharing, we'd like multiplication to work
+# as well, so we're actually operating in a ring.)
+# 
+# This is not a problem, because int64 is a finite ring! [2]
+# 
+# Another popular choice of a finite abelian ring is the integers modulo a prime [3], 
+# with the caveat that this doesn't support negative numbers. Thus, this implementation
+# defaults to using the int64 ring. We support prime rings as well, which are explicitly
+# used in the PrivateCompare algorithm.
+#
+# [1] 6.1 in https://cs.nyu.edu/courses/spring07/G22.3033-013/scribe/lecture01.pdf
+# [2] https://math.stackexchange.com/q/3692052/28855
+# [3] https://mortendahl.github.io/2017/09/03/the-spdz-protocol-part1/
+
+MAX_INT64 =  9223372036854775807
+MIN_INT64 = -9223372036854775808
+
+def mod(n, Q=None):
+    '''Keeps n inside the finite ring. That is:
+         - If we're in a prime ring (Q is the prime size), modulo it by Q
+         - If we're in the int64 ring, do the normal int64 overflow behavior
+           (we need to explicitly overflow since Python3 ints are unbounded)
+    '''
+    if Q is not None: return n % Q
+    return (n + MAX_INT64 + 1) % 2**64 - (MAX_INT64 + 1)  # https://stackoverflow.com/a/7771499/908744
+    
+def rand_element(Q=None):
+    '''Generates a random int64, or a random integer [0, Q) if Q is specified.
+       i.e. an element of the int64 ring, or the size-Q prime ring.'''
+    if Q is not None: return randrange(Q)
+    return randint(MIN_INT64, MAX_INT64)
 
 class VirtualMachine():
     '''A very simple class that represents a machine's data. 
@@ -21,22 +56,26 @@ class PrivateScalar():
         self.owner = owner
         owner.objects.append(self)
 
-    def share(self, machines, Q=Q):
+    def share(self, machines, Q=None):
         '''Generate additive secret shares of self.value, and distribute them to machines.'''
         # Make sure we're sharing across unique machines
         assert self.owner not in machines
-        assert len(machines) == len(set(machines))
+        assert len(machines) == len(set(machines))        
+        
+        # Make sure the value fits into the finite ring so it's possible to reconstruct it!
+        if Q is None: assert MIN_INT64 <= self.value <= MAX_INT64, f'{self} is not an int64 and cannot be reconstructed. Use a smaller value.'
+        else: assert 0 <= self.value < Q, f'{self} does not fit in this size-{Q} prime ring, so cannot be split into shares that can reconstruct it. Use a larger Q or a smaller value.'
         
         # Generate the value of each share using additive secret sharing
-        values = [randrange(Q) for _ in machines]
-        values.append((self.value - sum(values)) % Q)
-        
+        values = [rand_element(Q) for _ in machines]
+        values.append(mod(self.value - sum(values), Q))
+
         # Give one share to each machine
-        shares = [Share(value, machine, Q=Q) for value, machine in
+        shares = [Share(value, machine, Q) for value, machine in
                   zip(values, machines + [self.owner])]
-        
+
         # Return a SharedScalar that tracks all of these shares
-        return SharedScalar(shares, Q=Q)
+        return SharedScalar(shares, Q)
     
     def __repr__(self):
         return f'PrivateScalar({self.value}, \'{self.owner.name}\')'
@@ -44,8 +83,8 @@ class PrivateScalar():
 class Share():
     '''A class that represents a secret share that belongs to a machine.
        It supports normal arithmetic with other Shares or integers (+, -, *).'''
-    def __init__(self, value, owner, Q=Q):
-        assert 0 <= value < Q
+    def __init__(self, value, owner, Q=None):
+        if Q is not None: assert 0 <= value < Q        
         self.value = value
         self.owner = owner
         self.Q = Q
@@ -53,16 +92,29 @@ class Share():
     
     # Called by: self + other
     def __add__(self, other):
-        # Addition of a Share and a public integer known to all machines
-        if isinstance(other, int):
-            sum_value = (self.value + other) % self.Q
-            
-        # Addition of two Shares
-        elif isinstance(other, Share):
-            assert self.owner == other.owner
-            sum_value = (self.value + other.value) % self.Q
-            
-        return Share(sum_value, self.owner, self.Q)
+        if self.Q is None:
+            # Addition of a Share and a public integer known to all machines
+            if isinstance(other, int):
+                sum_value = mod(self.value + other)
+
+            # Addition of two Shares
+            elif isinstance(other, Share):
+                assert self.owner == other.owner
+                sum_value = mod(self.value + other.value)
+
+            return Share(sum_value, self.owner)
+        
+        else:
+            # Addition of a Share and a public integer known to all machines
+            if isinstance(other, int):
+                sum_value = mod(self.value + other, self.Q)
+
+            # Addition of two Shares
+            elif isinstance(other, Share):
+                assert self.owner == other.owner
+                sum_value = mod(self.value + other.value, self.Q)
+
+            return Share(sum_value, self.owner, self.Q)
     
     # Called by: other + self (when other is not a Share)
     def __radd__(self, other):
@@ -78,16 +130,29 @@ class Share():
     
     # Called by: self * other
     def __mul__(self, other):
-        # Multiplication of a Share and a public integer known to all machines
-        if isinstance(other, int):
-            prod_value = (self.value * other) % self.Q
-            
-        # Multiplication of two Shares
-        elif isinstance(other, Share):
-            assert self.owner == other.owner
-            prod_value = (self.value * other.value) % self.Q
-    
-        return Share(prod_value, self.owner, self.Q)
+        if self.Q is None:
+            # Multiplication of a Share and a public integer known to all machines
+            if isinstance(other, int):
+                prod_value = mod(self.value * other)
+
+            # Multiplication of two Shares
+            elif isinstance(other, Share):
+                assert self.owner == other.owner
+                prod_value = mod(self.value * other.value)
+
+            return Share(prod_value, self.owner)
+
+        else:
+            # Multiplication of a Share and a public integer known to all machines
+            if isinstance(other, int):
+                prod_value = mod(self.value * other, self.Q)
+
+            # Multiplication of two Shares
+            elif isinstance(other, Share):
+                assert self.owner == other.owner
+                prod_value = mod(self.value * other.value, self.Q)
+
+            return Share(prod_value, self.owner, self.Q)
     
     # Called by: other * self (when other is not a Share)  
     def __rmul__(self, other):
@@ -99,8 +164,8 @@ class Share():
 class SharedScalar():
     '''A class that tracks all secret shares that corresponds to one PrivateScalar.
        It supports *secure* arithmetic with other SharedScalars or integers (+, -, *).'''
-    def __init__(self, shares, Q=Q):
-        assert all(share.Q == Q for share in shares)
+    def __init__(self, shares, Q=None):
+        if Q is None: assert all(share.Q == Q for share in shares)
         self.shares = shares
         self.owners = {share.owner for share in shares}
         self.share_of = {share.owner: share for share in shares}
@@ -109,7 +174,10 @@ class SharedScalar():
     def reconstruct(self, owner):
         '''Send all secret shares to owner (can be anyone), and reconstruct the PrivateScalar value on that machine.'''
         values = [share.value for share in self.shares]
-        value = sum(values) % self.Q
+        if self.Q is None:
+            value = mod(sum(values))
+        else:
+            value = mod(sum(values), self.Q)
         return PrivateScalar(value, owner)
     
     # Called by: self + other
@@ -119,7 +187,7 @@ class SharedScalar():
             # To do the addition, we add the integer to one (random) share only
             new_share = self.shares[0] + other
             return SharedScalar([new_share] + self.shares[1:], Q=self.Q)
-        
+
         # Addition of two SharedScalars
         elif isinstance(other, SharedScalar):
             # To do the addition, we add each machine's shares together
@@ -155,8 +223,12 @@ class SharedScalar():
             assert self.owners == other.owners
 
             # Generate a random multiplication triple (public)
-            a, b = randrange(self.Q), randrange(self.Q)
-            c = (a * b) % self.Q
+            if self.Q is None:
+                a, b = randint(MIN_INT64, MAX_INT64), randint(MIN_INT64, MAX_INT64)
+                c = mod(a * b)
+            else:
+                a, b = randrange(self.Q), randrange(self.Q)
+                c = mod(a * b, self.Q)
 
             # Share the triple across machines
             rand_owner = choice(self.shares).owner
